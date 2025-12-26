9ca3c48ae1084239492e0f7c7e8840f254554cef
GaLiStudio <101331040+xiechengIt@users.noreply.github.com>
2025-12-26 19:21:13 +0800

    feat: 启用国际化功能并添加 Redis 缓存支持
    - 启用 i18n 国际化配置
    
    - 添加 cache-manager-ioredis-yet 依赖以支持 Redis 缓存

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -6,7 +6,7 @@ import { pCachePath, pUploadPath } from '../comm/path';
 import { availablePort } from '../comm/port';
 
 // redis缓存
-// import { redisStore } from 'cache-manager-ioredis-yet';
+import { redisStore } from 'cache-manager-ioredis-yet';
 
 export default {
   // 确保每个项目唯一，项目首次启动会自动生成
@@ -38,31 +38,31 @@ export default {
     whitelist: null,
   },
   // 缓存 可切换成其他缓存如：redis http://www.midwayjs.org/docs/extensions/caching
-  cacheManager: {
-    clients: {
-      default: {
-        store: CoolCacheStore,
-        options: {
-          path: pCachePath(),
-          ttl: 0,
-        },
-      },
-    },
-  },
   // cacheManager: {
   //   clients: {
   //     default: {
-  //       store: redisStore,
+  //       store: CoolCacheStore,
   //       options: {
-  //         port: 6379,
-  //         host: '127.0.0.1',
-  //         password: '',
+  //         path: pCachePath(),
   //         ttl: 0,
-  //         db: 0,
   //       },
   //     },
   //   },
   // },
+  cacheManager: {
+    clients: {
+      default: {
+        store: redisStore,
+        options: {
+          port: 6379,
+          host: '127.0.0.1',
+          password: '',
+          ttl: 0,
+          db: 0,
+        },
+      },
+    },
+  },
   cool: {
     // 已经插件化，本地文件上传查看 plugin/config.ts，其他云存储查看对应插件的使用
     file: {},
@@ -76,7 +76,7 @@ export default {
     // 国际化配置
     i18n: {
       // 是否开启
-      enable: false,
+      enable: true,
       // 语言
       languages: ['zh-cn', 'zh-tw', 'en'],
     },
